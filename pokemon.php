5839df3c1ee7472f17c80d9a6217f6bded6b39bd
jimtahu <jimtahu@gmail.com>
2012-08-06 14:24:27 -0400

    Added php class matching schema for pokemon

---
+++ pokemon.php
@@ -0,0 +1,18 @@
+<?php
+class pokemon {
+	//from pokemon
+	public $Name = 'noone';
+	public $Level = 1;
+	public $Type = 'NONE';
+	public $Ability = ' nothing ';
+	public $Dexno = 0;
+	public $Gender = 'None';
+	//from stats
+	public $HP = 0;
+	public $Speed = 0;
+	public $Attack = 0;
+	public $Defense = 0;
+	public $SPAttack = 0;
+	public $SPDefense = 0;
+}
+?>

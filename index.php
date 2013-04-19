c1fa1fe6d0ef6bb100d85e00210c780345fcdf2e
jimtahu <jimtahu@gmail.com>
2013-04-19 18:26:09 -0500

    Some nice upgrades

--- index.php
+++ index.php
@@ -3,12 +3,18 @@ include_once('head.php');
 include_once('pokemon.php');
 ?>
 <h3>Pokemon</h3>
-<ul>
+<table>
 <?php
 $list = listPokemon();
 foreach($list as $name){
-	echo "<li><a href=pokeview.php?act=view&name=$name>$name</a></li>";
+	echo "<tr>";
+    echo "<td><a href=pokeview.php?act=view&name=$name>$name</a></td>";
+    echo "<td><form action='pokeview.php'>
+          <input type='hidden' name='act' value='edit'>
+          <input type='hidden' name='name' value='$name'>
+          <input value='Edit' type='submit' ></form></td>";
+    echo "</tr>";
 }
 ?>
-</ul>
+</table>
 <?php include_once('tail.php'); ?>
